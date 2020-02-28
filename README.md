cf485f99af56009fa893bc8b812d73d212e53a14
richen <richenlin@gmail.com>
2020-02-28 16:10:36 +0800

    init

--- README.md
+++ README.md
@@ -34,14 +34,16 @@ list: [...,'apollo'], //加载的中间件列表
 config: { //中间件配置
     ...,
     apollo: {
-        config_path: process.env.APP_PATH + '/config', //project configs path
-        apollo_server: 'http://127.0.0.18080', //your-config-server-url
-        apollo_appid: { cache: true }, //your-config-appid
-        apollo_cluster: 'default', //default to `default`
-        apollo_namespaces: ['application'], //default to `['application']`, this is the namespaces that you want to use or maintain.
-        initial_configs: true, //default to true
-        apollo_listen_notify: 'default', //default to true
-        apollo_fetch_interval: 5 * 60e3, //default to 5 minutes. can be customize but 30s or shorter time are not acceptable.
+        config_key: 'db', // Apollo configuration will overwrite the configuration key, the default value is 'db', corresponding to the configuration file 'db.ts'
+        host: 'http://127.0.0.1:8080', //your-config-server-url
+        appId: 'test', //your-config-appId
+        clusterName: 'default', //cluster-name
+        namespace: 'application', //your-config-namespace
+        enableUpdateNotification: true, // boolean=true set to false to disable update notification.
+        enableFetch: true, //boolean=false set to true to enable the feature
+        fetchTimeout: 30000, // number=30000 timeout in milliseconds before the initial fetch or interval fetch result in an FETCH_TIMEOUT error.
+        fetchInterval: 5 * 60 * 1000, // number = 5 * 60 * 1000 interval in milliseconds to pull the new configurations.Defaults to 5 minutes.Setting this option to 0 will disable the feature.
+        fetchCachedConfig: true, // boolean = true whether refresh configurations by fetching the restful API with caches.Defaults to true.If you want to always fetch the latest configurations(not recommend), set the option to false
     }
 }
 ```
@@ -75,13 +77,15 @@ list: [...,'apollo'], //加载的中间件列表
 config: { //中间件配置
     ...,
     apollo: {
-        config_path: process.env.APP_PATH + '/config', //project configs path
-        apollo_server: 'http://127.0.0.18080', //your-config-server-url
-        apollo_appid: { cache: true }, //your-config-appid
-        apollo_cluster: 'default', //default to `default`
-        apollo_namespaces: ['application'], //default to `['application']`, this is the namespaces that you want to use or maintain.
-        initial_configs: true, //default to true
-        apollo_listen_notify: 'default', //default to true
-        apollo_fetch_interval: 5 * 60e3, //default to 5 minutes. can be customize but 30s or shorter time are not acceptable.
+        config_key: 'db', // Apollo configuration will overwrite the configuration key, the default value is 'db', corresponding to the configuration file 'db.ts'
+        host: 'http://127.0.0.1:8080', //your-config-server-url
+        appId: 'test', //your-config-appId
+        clusterName: 'default', //cluster-name
+        namespace: 'application', //your-config-namespace
+        enableUpdateNotification: true, // boolean=true set to false to disable update notification.
+        enableFetch: true, //boolean=false set to true to enable the feature
+        fetchTimeout: 30000, // number=30000 timeout in milliseconds before the initial fetch or interval fetch result in an FETCH_TIMEOUT error.
+        fetchInterval: 5 * 60 * 1000, // number = 5 * 60 * 1000 interval in milliseconds to pull the new configurations.Defaults to 5 minutes.Setting this option to 0 will disable the feature.
+        fetchCachedConfig: true, // boolean = true whether refresh configurations by fetching the restful API with caches.Defaults to true.If you want to always fetch the latest configurations(not recommend), set the option to false
     }
 }
--- index.js
+++ index.js
@@ -5,69 +5,83 @@
  * @license    MIT
  * @version    17/6/12
  */
-const apollo = require('node-apollo-client')
-const globby = require("globby");
+const apollo = require('ctrip-apollo')
+const lib = require("think_lib");
+const logger = require("think_logger");
 
 /**
  * default options
  */
 const defaultOptions = {
-    config_path: process.env.APP_PATH + '/config', //project configs path
-    apollo_server: 'http://127.0.0.18080', //your-config-server-url
-    apollo_appid: { cache: true }, //your-config-appid
-    apollo_cluster: 'default', //default to `default`
-    apollo_namespaces: ['application'], //default to `['application']`, this is the namespaces that you want to use or maintain.
-    initial_configs: true, //default to true
-    apollo_listen_notify: 'default', //default to true
-    apollo_fetch_interval: 5 * 60e3, //default to 5 minutes. can be customize but 30s or shorter time are not acceptable.
+    config_key: 'db', // Apollo configuration will overwrite the configuration key, the default value is 'db', corresponding to the configuration file 'db.ts'
+    host: 'http://127.0.0.1:8080', //your-config-server-url
+    appId: 'test', //your-config-appId
+    clusterName: 'default', //cluster-name
+    namespace: 'application', //your-config-namespace
+    enableUpdateNotification: true, // boolean=true set to false to disable update notification.
+    enableFetch: true, //boolean=false set to true to enable the feature
+    fetchTimeout: 30000, // number=30000 timeout in milliseconds before the initial fetch or interval fetch result in an FETCH_TIMEOUT error.
+    fetchInterval: 5 * 60 * 1000, // number = 5 * 60 * 1000 interval in milliseconds to pull the new configurations.Defaults to 5 minutes.Setting this option to 0 will disable the feature.
+    fetchCachedConfig: true, // boolean = true whether refresh configurations by fetching the restful API with caches.Defaults to true.If you want to always fetch the latest configurations(not recommend), set the option to false
+    // cachePath: process.env.APP_PATH + "/config/"
 };
 
 module.exports = function (options, app) {
     options = options ? lib.extend(defaultOptions, options, true) : defaultOptions;
-    const apolloConifgs = {
-        configServerUrl: options.apollo_server,
-        appId: options.apollo_appid,
-        cluster: options.apollo_cluster, // [optional] default to `default`
-        listenOnNotification: options.apollo_listen_notify, // [optional] default to true
-        fetchCacheInterval: options.apollo_fetch_interval, // [optional] default to 5 minutes. can be customize but 30s or shorter time are not acceptable.
-    };
-    if (options.initial_configs) {
-        const fileResults = globby.sync(['**/**.js', '**/**.ts'], {
-            cwd: options.config_path
-        });
-        apolloConifgs.namespaces = [];
-        apolloConifgs.initialConfigs = {};
-        for (let name of fileResults) {
-            if (name.indexOf('/') > -1) {
-                name = name.slice(name.lastIndexOf('/') + 1);
-            }
-            const fileName = name.slice(0, -3);
-            apolloConifgs.namespaces.push(fileName);
-            apolloConifgs.initialConfigs[fileName] = app.config(undefined, fileName);
+
+    /**
+     * 
+     *
+     * @param {*} key
+     * @param {*} newConfig
+     * @returns
+     */
+    const reFreshConfig = function (key, newConfig) {
+        if (lib.isEmpty(newConfig)) {
+            return;
+        }
+        let appConfig = app.config(undefined, key);
+        appConfig = Object.assign(appConfig, newConfig);
+        if (app.setMap) {
+            app.setMap(key, appConfig)
+        } else if (app._caches.configs) {
+            !app._caches.configs[key] && (app._caches.configs[key] = {});
+            app._caches.configs[key] = appConfig;
         }
     }
-    apolloConifgs.namespaces = [];
-    apolloConifgs.initialConfigs = {
-        application: {}
-    };
 
+    /**
+     *
+     *
+     * @param {*} opt
+     * @param {*} app
+     */
     const initApolo = async function (opt, app) {
         // Instantiate Apollo
-        const apolloClient = new apollo(opt);
-        const ps = [];
-        opt.apollo_namespaces.map(item => {
-            ps.push(apolloClient.fetchConfigsFromCache(item).then(res => {
-                if (app.setMap) {
-                    app.setMap(item, res);
-                } else if (app._caches.configs) {
-                    app._caches.configs = res;
-                }
-            }));
+        const apolloClient = apollo(opt).namespace(opt.namespace, 'JSON');
+
+        if (opt.enableUpdateNotification) {
+            apolloClient.on('change', ({
+                key,
+                oldValue,
+                newValue
+            }) => {
+                logger.info(`Apollo configuration key ${key} has been refreshed. the new values is ${newValue}`);
+                reFreshConfig(opt.config_key, { [key]: newValue });
+            });
+        }
+
+        const ns = await apolloClient.ready().catch(err => {
+            return Promise.reject(err);
         });
-        return Promise.all(ps);
+        logger.info('Apollo configuration center initialization is complete.');
+        const apolloConfigs = await ns.config() || {};
+        if (!lib.isEmpty(apolloConfigs)) {
+            reFreshConfig(opt.config_key, apolloConfigs);
+        }
     }
-    app.once('appReady', () => {
-        initApolo();
+    app.once('appReady', async () => {
+        await initApolo(options, app);
     });
 
     return function (ctx, next) {
